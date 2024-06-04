133124d81e57aa590b213baa1965684dfab2dec4
CYG <cyg@MAGIT03641.local>
2024-06-04 19:22:48 +0800

    syd ktv api

--- KTVAPI/Android/app/src/main/java/io/agora/ktvdemo/rtc/RtcEngineController.kt
+++ KTVAPI/Android/app/src/main/java/io/agora/ktvdemo/rtc/RtcEngineController.kt
@@ -25,7 +25,7 @@ object RtcEngineController {
             if (innerRtcEngine == null) {
                 val config = RtcEngineConfig()
                 config.mContext = MyApplication.app()
-                config.mAppId = "*****************************"
+                config.mAppId = BuildConfig.AGORA_APP_ID
                 config.mEventHandler = object : IRtcEngineEventHandler() {
                     override fun onError(err: Int) {
                         super.onError(err)
@@ -50,8 +50,8 @@ object RtcEngineController {
             return innerRtcEngine!!
         }
 
-    var chorusChannelRtcToken = ""
-    var audienceChannelToken = ""
+    var audienceChannelToken: String = ""
+    var chorusChannelToken: String = ""
     var musicStreamToken = ""
     var rtmToken = ""
 }
